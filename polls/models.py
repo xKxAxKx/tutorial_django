5eb90ac5faa74d6ac8134786ff75acc92816def4
xKxAxKx <pekepooooong@gmail.com>
2017-01-15 16:25:13 +0900

    edit model.py

--- polls/models.py
+++ polls/models.py
@@ -15,5 +15,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
